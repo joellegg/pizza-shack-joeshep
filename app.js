5398a4607419fbc1c1e18a2357e958328ea213c2
joellegg <joellegg@bellsouth.net>
2017-03-30 10:11:12 -0500

    added navbar and contact page

--- app.js
+++ app.js
@@ -7,12 +7,27 @@ const app = express();
 // pug configuration
 app.set('view engine', 'pug');
 
+app.locals.company = "🍕 Pizza Shack 🍕"
+app.locals.body = {};
+app.locals.body.magic = "Pizzzzzaaaaa!";
+
 // Middleware
 app.use(express.static('public'));
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+app.get('/about', (req, res, next) => {
+  res.render('about', {page: 'About'});
+});
+app.get('/contact', (req, res, next) => {
+  res.render('contact');
 })
+app.use((req, res) => {
+  res.render('404')
+})
+// ********* end of middleware ***************
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {console.log(`listening on ${port}`)})
